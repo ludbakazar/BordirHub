63ba4ab1fea3a17ed17027e6294ef80f2825fd83
ludbakazar <ludbakazarr@gmail.com>
2025-02-13 09:43:00 +0700

    Add Services page with redirect to create service functionality

---
+++ bordirhub/src/app/services/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { redirect } from "next/navigation";
+
+export default function Services() {
+  return (
+    <div className="h-screen ">
+      <div className="grid grid-cols-3 gap-4 py-20 px-20">
+        <div
+          onClick={() => redirect("/services/create")}
+          className="cursor-pointer transform transition-transform duration-300 hover:scale-105"
+        >
+          <div className="card bg-base-100 text-primary-content w-96 h-48 shadow-lg hover:shadow-xl transition-shadow duration-300">
+            <div className="card-body flex items-center justify-center">
+              <h2 className="card-title text-white text-2xl font-bold">
+                TAMBAH SERVICE
+              </h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
